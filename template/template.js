1a18575e2bb2953c811ae65e68ab308f1cccba14
geregere <geregere@kcd.biglobe.ne.jp>
2020-08-22 11:59:52 +0900

    初回コミット

---
+++ template/template.js
@@ -0,0 +1,67 @@
+//=============================================================================
+// Plugin for RPG Maker MZ
+// ******.js
+// ----------------------------------------------------------------------------
+// Released under the MIT License.
+// https://opensource.org/licenses/mit-license.php
+//=============================================================================
+/*:ja
+ * @target MZ
+ * @plugindesc 
+ * @author げれげれ
+ * @url https://twitter.com/geregeregere
+ *
+ * 
+ * @param 
+ * @text 
+ * @desc 
+ * @default
+ * @type 
+ * @max 
+ * @min 
+ * @on 
+ * @off 
+ * @parent 
+ *
+ * @command 
+ * @text 
+ * @desc 
+ * 
+ * @arg 
+ * @type 
+ * @text 
+ * @desc 
+ * 
+ * 
+ * @help
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+(() => {
+    'use strict';
+    const pluginName = '******';
+
+    //プラグインパラメータ=================================================
+    const parameters = PluginManager.parameters(pluginName);
+    //以後、parameters['パラメータ名']の形で値を取得していく
+
+
+
+    //プラグインコマンド===================================================
+    PluginManager.registerCommand(pluginName, "", args => {
+
+    });
+
+    //実処理==============================================================
+
+
+
+
+
+
+})();
